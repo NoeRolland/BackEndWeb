77c4ca48bd240e68c727e8e0cb8d6442e7703084
maneleaplaza <mrleaplaza@gmail.com>
2025-02-28 13:07:09 +0100

    clean file

--- models/Icecream.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const TacheSchema = new mongoose.Schema({
-    Commande : { type: String, required: true },
-    Specifications: { type: String },
-    dateCreation: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Tache', TacheSchema);
--- models/Tache.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const TacheSchema = new mongoose.Schema({
-    titre: { type: String, required: true },
-    description: { type: String },
-    dateCreation: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Tache', TacheSchema);
